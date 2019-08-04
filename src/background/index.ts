4673c0977dc0a53af9190aa0dd928c9b2c4f2686
Sential <sentialx@gmail.com>
2019-08-04 22:41:21 +0200

    chore: don't open url on install

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,34 +1,32 @@
-import {Extension} from './extension';
-import {getHelpURL} from '../utils/links';
+import { Extension } from "./extension";
+import { getHelpURL } from "../utils/links";
 
 // Initialize extension
 const extension = new Extension();
 extension.start();
 
-chrome.runtime.onInstalled.addListener(({reason}) => {
-    if (reason === 'install') {
-        chrome.tabs.create({url: getHelpURL()});
-    }
-});
-
 declare const __DEBUG__: boolean;
 const DEBUG = __DEBUG__;
 
 if (DEBUG) {
-    // Reload extension on connection
-    const listen = () => {
-        const req = new XMLHttpRequest();
-        req.open('GET', 'http://localhost:8890/', true);
-        req.overrideMimeType('text/plain');
-        req.onload = () => {
-            if (req.status >= 200 && req.status < 300 && req.responseText === 'reload') {
-                chrome.runtime.reload();
-            } else {
-                setTimeout(listen, 2000);
-            }
-        };
-        req.onerror = () => setTimeout(listen, 2000);
-        req.send();
+  // Reload extension on connection
+  const listen = () => {
+    const req = new XMLHttpRequest();
+    req.open("GET", "http://localhost:8890/", true);
+    req.overrideMimeType("text/plain");
+    req.onload = () => {
+      if (
+        req.status >= 200 &&
+        req.status < 300 &&
+        req.responseText === "reload"
+      ) {
+        chrome.runtime.reload();
+      } else {
+        setTimeout(listen, 2000);
+      }
     };
-    setTimeout(listen, 2000);
+    req.onerror = () => setTimeout(listen, 2000);
+    req.send();
+  };
+  setTimeout(listen, 2000);
 }
--- src/background/messenger.ts
+++ src/background/messenger.ts
@@ -1,107 +1,77 @@
-import {ExtensionData, FilterConfig, TabInfo, Message, UserSettings} from '../definitions';
+import {
+  ExtensionData,
+  FilterConfig,
+  TabInfo,
+  Message,
+  UserSettings
+} from "../definitions";
 
 interface ExtensionAdapter {
-    collect: () => Promise<ExtensionData>;
-    getActiveTabInfo: () => Promise<TabInfo>;
-    changeSettings: (settings: Partial<UserSettings>) => void;
-    setTheme: (theme: Partial<FilterConfig>) => void;
-    setShortcut: ({command, shortcut}) => void;
-    markNewsAsRead: (ids: string[]) => void;
-    toggleSitePattern: (pattern: string) => void;
-    onPopupOpen: () => void;
-    applyDevDynamicThemeFixes: (json: string) => Error;
-    resetDevDynamicThemeFixes: () => void;
-    applyDevInversionFixes: (json: string) => Error;
-    resetDevInversionFixes: () => void;
-    applyDevStaticThemes: (text: string) => Error;
-    resetDevStaticThemes: () => void;
+  collect: () => Promise<ExtensionData>;
+  getActiveTabInfo: () => Promise<TabInfo>;
+  changeSettings: (settings: Partial<UserSettings>) => void;
+  setTheme: (theme: Partial<FilterConfig>) => void;
+  setShortcut: ({ command, shortcut }) => void;
+  toggleSitePattern: (pattern: string) => void;
+  onPopupOpen: () => void;
 }
 
 export default class Messenger {
-    private reporters: Set<(info: ExtensionData) => void>;
-    private adapter: ExtensionAdapter;
+  private reporters: Set<(info: ExtensionData) => void>;
+  private adapter: ExtensionAdapter;
 
-    constructor(adapter: ExtensionAdapter) {
-        this.reporters = new Set();
-        this.adapter = adapter;
-        chrome.runtime.onConnect.addListener((port) => {
-            if (port.name === 'ui') {
-                port.onMessage.addListener((message) => this.onUIMessage(port, message));
-                this.adapter.onPopupOpen();
-            }
-        });
+  constructor(adapter: ExtensionAdapter) {
+    this.reporters = new Set();
+    this.adapter = adapter;
+    chrome.runtime.onConnect.addListener(port => {
+      if (port.name === "ui") {
+        port.onMessage.addListener(message => this.onUIMessage(port, message));
+        this.adapter.onPopupOpen();
+      }
+    });
+  }
+
+  private async onUIMessage(
+    port: chrome.runtime.Port,
+    { type, id, data }: Message
+  ) {
+    switch (type) {
+      case "get-data": {
+        const data = await this.adapter.collect();
+        port.postMessage({ id, data });
+        break;
+      }
+      case "get-active-tab-info": {
+        const data = await this.adapter.getActiveTabInfo();
+        port.postMessage({ id, data });
+        break;
+      }
+      case "subscribe-to-changes": {
+        const report = data => port.postMessage({ id, data });
+        this.reporters.add(report);
+        port.onDisconnect.addListener(() => this.reporters.delete(report));
+        break;
+      }
+      case "change-settings": {
+        this.adapter.changeSettings(data);
+        break;
+      }
+      case "set-theme": {
+        this.adapter.setTheme(data);
+        break;
+      }
+      case "set-shortcut": {
+        this.adapter.setShortcut(data);
+        break;
+      }
+      case "toggle-site-pattern": {
+        this.adapter.toggleSitePattern(data);
+        break;
+      }
     }
+  }
 
-    private async onUIMessage(port: chrome.runtime.Port, {type, id, data}: Message) {
-        switch (type) {
-            case 'get-data': {
-                const data = await this.adapter.collect();
-                port.postMessage({id, data});
-                break;
-            }
-            case 'get-active-tab-info': {
-                const data = await this.adapter.getActiveTabInfo();
-                port.postMessage({id, data});
-                break;
-            }
-            case 'subscribe-to-changes': {
-                const report = (data) => port.postMessage({id, data});
-                this.reporters.add(report);
-                port.onDisconnect.addListener(() => this.reporters.delete(report));
-                break;
-            }
-            case 'change-settings': {
-                this.adapter.changeSettings(data);
-                break;
-            }
-            case 'set-theme': {
-                this.adapter.setTheme(data);
-                break;
-            }
-            case 'set-shortcut': {
-                this.adapter.setShortcut(data);
-                break;
-            }
-            case 'toggle-site-pattern': {
-                this.adapter.toggleSitePattern(data);
-                break;
-            }
-            case 'mark-news-as-read': {
-                this.adapter.markNewsAsRead(data);
-                break;
-            }
-
-            case 'apply-dev-dynamic-theme-fixes': {
-                const error = this.adapter.applyDevDynamicThemeFixes(data);
-                port.postMessage({id, error: (error ? error.message : null)});
-                break;
-            }
-            case 'reset-dev-dynamic-theme-fixes': {
-                this.adapter.resetDevDynamicThemeFixes();
-                break;
-            }
-            case 'apply-dev-inversion-fixes': {
-                const error = this.adapter.applyDevInversionFixes(data);
-                port.postMessage({id, error: (error ? error.message : null)});
-                break;
-            }
-            case 'reset-dev-inversion-fixes': {
-                this.adapter.resetDevInversionFixes();
-                break;
-            }
-            case 'apply-dev-static-themes': {
-                const error = this.adapter.applyDevStaticThemes(data);
-                port.postMessage({id, error: error ? error.message : null});
-                break;
-            }
-            case 'reset-dev-static-themes': {
-                this.adapter.resetDevStaticThemes();
-                break;
-            }
-        }
-    }
-
-    reportChanges(data: ExtensionData) {
-        this.reporters.forEach((report) => report(data));
-    }
+  reportChanges(data: ExtensionData) {
+    this.reporters.forEach(report => report(data));
+  }
 }
